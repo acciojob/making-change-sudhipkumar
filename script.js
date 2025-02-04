a3c04e31f4dbbf136ee9bca36ee0c56642f204ee
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-02-04 19:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(cents) {
+    let change = {
+        'q': 0,
+        'd': 0,
+        'n': 0,
+        'p': 0
+    };
 
-// Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+    // calculate quarters
+    change.q = Math.floor(cents / 25);
+    cents = cents % 25;
+
+    // calculate dimes
+    // calculate nickels
+    // calculate pennies
+
+    return change;
+}
